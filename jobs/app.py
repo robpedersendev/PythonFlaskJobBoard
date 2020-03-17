254bb71b0e8c0b98bb5a4c5d850dbccefb14595c
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-03-17 16:37:06 -0600

    Addred route handler
    Added the route for employer reviews that will list all the jobs and the reviews of the respective company

--- jobs/app.py
+++ jobs/app.py
@@ -39,6 +39,14 @@ def close_connection(exception):
         connection.close()
 
 
+@app.route('/employer/<employer_id>')
+def employer(employer_id):
+    employer = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single=True)
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
+    reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
+    return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
+
+
 @app.route('/job/<job_id>')
 def job(job_id):
     job = execute_sql(
